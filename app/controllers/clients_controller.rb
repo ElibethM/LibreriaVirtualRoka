a64c26fdf2169af6b8260e5947249eae1e2fc681
ElibethM <Elibeth_Magno@hotmail.com>
2014-07-05 17:00:40 -0500

    first commit

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -27,6 +27,7 @@ class ClientsController < ApplicationController
     @client = Client.new(client_params)
 
     respond_to do |format|
+      if @client.valid?
       if @client.save
         format.html { redirect_to @client, notice: 'Client was successfully created.' }
         format.json { render :show, status: :created, location: @client }
@@ -34,8 +35,12 @@ class ClientsController < ApplicationController
         format.html { render :new }
         format.json { render json: @client.errors, status: :unprocessable_entity }
       end
+    else
+      format.html { render :new}
+      format.json {render json: @client.errors, status: :unprocessable_entity}
     end
   end
+end
 
   # PATCH/PUT /clients/1
   # PATCH/PUT /clients/1.json
--- app/models/client.rb
+++ app/models/client.rb
@@ -1,2 +1,11 @@
 class Client < ActiveRecord::Base
+	has_many :sales
+    validates :rfc, :presence => { message: "No puede dejarse vacío" }
+	validates :nombre, :presence => { message: "No puede dejarse vacío" }
+	validates :telefono, :presence => { message: "No puede dejarse vacío" }
+	validates :email, :presence => { message: "No puede dejarse vacío" }
+	validates :direccion, :presence => { message: "No puede dejarse vacío" }
+	validates :facebook, :presence => { message: "No puede dejarse vacío" }
+	validates :linkedIn, :presence => { message: "No puede dejarse vacío" }
+
 end
